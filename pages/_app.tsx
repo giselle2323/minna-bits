a3a7ecaf8465a0df43a493b49a6a3f0d52cead61
minna <yakubuaminat94@gmail.com>
2022-02-01 17:45:59 +0100

    docs: initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+
+import type { AppProps } from 'next/app'
+import type { NextPage } from 'next'
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+  return getLayout(<Component {...pageProps} />);
+}
---
+++ pages/index.tsx
@@ -0,0 +1,152 @@
+import Layout from '../app/layout'
+
+export default function Dashboard() {
+  return (
+    <div>
+      <div className="mb-10 grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <div className="overflow-hidden border border-gray-200 bg-white p-3 hover:bg-green-100">
+            <div className="m-2 text-justify text-sm">
+              <p className="text-right text-xs">May 17, 2020</p>
+              <h2 className="mb-8 h-2 text-lg font-bold">Blog Post Title </h2>
+              <p className="text-xs">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum. djnkf kdsnfkjnf
+              </p>
+            </div>
+            <div className="mt-4 w-full text-right">
+              <a
+                className="text-sm font-bold uppercase text-green-400"
+                href="#"
+              >
+                Read More
+              </a>
+            </div>
+          </div>
+          <div className="overflow-hidden border border-gray-200 bg-white p-3 hover:bg-green-100">
+            <div className="m-2 text-justify text-sm">
+              <p className="text-right text-xs">May 17, 2020</p>
+              <h2 className="mb-8 h-2 text-lg font-bold">Blog Post Title </h2>
+              <p className="text-xs">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div className="mt-4 w-full text-right">
+              <a
+                className="text-sm font-bold uppercase text-green-400"
+                href="#"
+              >
+                Read More
+              </a>
+            </div>
+          </div>
+          <div className="overflow-hidden border border-gray-200 bg-white p-3 hover:bg-green-100">
+            <div className="m-2 text-justify text-sm">
+              <p className="text-right text-xs">May 17, 2020</p>
+              <h2 className="mb-8 h-2 text-lg font-bold">Blog Post Title </h2>
+              <p className="text-xs">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div className="mt-4 w-full text-right">
+              <a
+                className="text-sm font-bold uppercase text-green-400"
+                href="#"
+              >
+                Read More
+              </a>
+            </div>
+          </div>
+          <div className="overflow-hidden border border-gray-200 bg-white p-3 hover:bg-green-100">
+            <div className="m-2 text-justify text-sm">
+              <p className="text-right text-xs">May 17, 2020</p>
+              <h2 className="mb-8 h-2 text-lg font-bold">Blog Post Title </h2>
+              <p className="text-xs">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div className="mt-4 w-full text-right">
+              <a
+                className="text-sm font-bold uppercase text-green-400"
+                href="#"
+              >
+                Read More
+              </a>
+            </div>
+          </div>
+          <div className="overflow-hidden border border-gray-200 bg-white p-3 hover:bg-green-100">
+            <div className="m-2 text-justify text-sm">
+              <p className="text-right text-xs">May 17, 2020</p>
+              <h2 className="mb-8 h-2 text-lg font-bold">Blog Post Title </h2>
+              <p className="text-xs">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div className="mt-4 w-full text-right">
+              <a
+                className="text-sm font-bold uppercase text-green-400"
+                href="#"
+              >
+                Read More
+              </a>
+            </div>
+          </div>
+          <div className="overflow-hidden border border-gray-200 bg-white p-3 hover:bg-green-100">
+            <div className="m-2 text-justify text-sm">
+              <p className="text-right text-xs">May 17, 2020</p>
+              <h2 className="mb-8 h-2 text-lg font-bold">Blog Post Title </h2>
+              <p className="text-xs">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div className="mt-4 w-full text-right">
+              <a
+                className="text-sm font-bold uppercase text-green-400"
+                href="#"
+              >
+                Read More
+              </a>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+Dashboard.getLayout = function getLayout(page: React.ReactElement) {
+  return <Layout>{page}</Layout>
+}
